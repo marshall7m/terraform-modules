0e023cf2615d1fb11fa45ab092e84545d22ef5fb
marshall7m <marshallmamiya@gmail.com>
2020-10-16 20:58:23 -0700

    change parameter store key name for airflow meta-db

--- terraform-aws-airflow/outputs.tf
+++ terraform-aws-airflow/outputs.tf
@@ -6,7 +6,11 @@ output "airflow_ec2_id" {
     value = "${var.create_airflow_instance ? aws_instance.airflow[0].id : null}"
 }
 
-output "airflow_rds_id" {
+output "airflow_db_id" {
     value = "${var.create_airflow_db ? aws_db_instance.airflow[0].id : null}"
 }
 
+output "airflow_db_conn_name" {
+    description = "Airflow meta-db connection string KEY name (not sensitive) in SSM parameter store."
+    value = aws_ssm_parameter.AIRFLOW__CORE__SQL_ALCHEMY_CONN.name
+}
