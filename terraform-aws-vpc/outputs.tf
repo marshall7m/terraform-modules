52d217aad0022faeaaeccdee6371ce844b4e40ea
marshall7m <marshallmamiya@gmail.com>
2020-11-21 14:08:24 -0800

    revert to orignal db subnet output

--- terraform-aws-vpc/outputs.tf
+++ terraform-aws-vpc/outputs.tf
@@ -16,7 +16,7 @@ output "private_subnets_arns" {
 }
 
 output "database_subnets_ids" {
-    value = module.vpc.outputs.database_subnets
+    value = module.vpc.database_subnets
 }
 
 output "database_subnets_cidr_blocks" {
