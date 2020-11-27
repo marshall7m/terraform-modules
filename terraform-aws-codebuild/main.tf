fe151d8147ac7d54bad8e41be934f920e61dc625
marshall7m <marshallmamiya@gmail.com>
2020-11-27 13:07:20 -0800

    add multi/single build webhook options

--- terraform-aws-codebuild/main.tf
+++ terraform-aws-codebuild/main.tf
@@ -1,27 +1,30 @@
 resource "aws_codebuild_project" "infrastructure" {
-  for_each = {for build in var.builds: build.name => build}
-  name          = "${var.resource_prefix}-${each.value.name}-${var.resource_suffix}"
-  description   = "Executes the following commands: [${join(", ", each.value.commands)}] within the following target directories: [${join(", ", try(each.value.target_paths, var.target_paths))}]"
+  for_each = var.builds != null ? {for build in var.builds: build.name => build} : tomap({"" = {}})
+  name          = try("${var.resource_prefix}-${each.key}-${var.resource_suffix}", "${var.resource_prefix}-${var.name}-${var.resource_suffix}")
+  description   = try("Executes the following commands: [${join(", ", each.value.commands)}] within the following target directories: [${join(", ", try(each.value.target_paths, var.target_paths))}]", "Template build project for terraformed infrastructure")
   build_timeout = "5"
-  service_role  = each.value.service_role_arn
+  service_role  = try(each.value.service_role_arn, var.service_role_arn)
 
   dynamic "artifacts" {
-      for_each = each.value.artifacts
-      content {
-          type = artifacts.value
-      }
+    for_each = try(each.value.artifacts, [var.artifacts])
+    content {
+      type = artifacts.value.type
+      artifact_identifier = try(artifacts.value.artifact_identifier, null)
+      encryption_disabled = try(artifacts.value.encryption_disabled, null)
+      override_artifact_name = try(artifacts.value.override_artifact_name, null)
+      location = try(artifacts.value.location, null)
+      name = try(artifacts.value.name, null)
+      namespace_type = try(artifacts.value.namespace_type, null)
+      packaging = try(artifacts.value.packaging, null)
+      path = try(artifacts.value.path, null)
+    }
   }
 
   environment {
-    compute_type                = try(each.value.build_compute_type, "BUILD_GENERAL1_SMALL")
-    image                       = try(each.value.build_image, "aws/codebuild/standard:4.0")
-    type                        = try(each.value.build_type, "LINUX_CONTAINER")
-    image_pull_credentials_type = try(each.value.image_pull_credentials_type, "CODEBUILD")
-
-    environment_variable {
-      name  = "COMMANDS"
-      value = join(" && ", each.value.commands)
-    }
+    compute_type                = try(each.value.build_compute_type, var.build_compute_type)
+    image                       = try(each.value.build_image, var.build_image)
+    type                        = try(each.value.build_type, var.build_type)
+    image_pull_credentials_type = try(each.value.build_image_pull_credentials_type, var.build_image_pull_credentials_type)
 
     environment_variable {
       name  = "TERRAFORM_VERSION"
@@ -44,28 +47,45 @@ resource "aws_codebuild_project" "infrastructure" {
     }
 
     dynamic "environment_variable" {
-      for_each = each.value.target_paths != null || var.target_paths != null ? [1] : []
-      name  = "TARGET_DIRS"
-      value = join(" ", try(each.value.target_paths, var.target_paths))
+      for_each = try(each.value.target_paths, var.target_paths) != null ? [1] : []
+      content {
+        name  = "TARGET_DIRS"
+        value = join(" ", try(each.value.target_paths, var.target_paths))
+      }
     }
 
     dynamic "environment_variable" {
-        for_each = try(each.value.environment_variables, {}) 
+      for_each = try(each.value.commands, var.commands) != null ? [1] : []
+      content {
+        name  = "COMMANDS"
+        value = join(" && ", try(each.value.commands, var.commands))
+      }
+    }
+
+    dynamic "environment_variable" {
+        for_each = {for env_var in try(each.value.environment_variables, var.environment_variables): env_var.name => env_var}
         content {
-            name  = environment_variable.key
-            value = environment_variable.value
+            name  = environment_variable.value.name
+            value = environment_variable.value.value
+            type = "PLAINTEXT"
         }
     }
   }
   
-  dynamic "logs_config" {
-    for_each = try(each.value.s3_log_path, null) != null ? [1] : []
-    content {
-      s3_logs {
-        status   = "ENABLED"
-        location = each.value.s3_log_path
-      }
+  logs_config {
+    cloudwatch_logs {
+      status = "ENABLED"
+      stream_name = try(each.value.cw_stream_name, var.cw_stream_name)
+      group_name = try(each.value.cw_group_name, var.cw_group_name)
     }
+    dynamic "s3_logs" {
+      for_each = try(each.value.s3_log_path, var.s3_log_path) != null ? [1] : []
+      content {
+        status   = "ENABLED"
+        location = try(each.value.s3_log_path, var.s3_log_path)
+        encryption_disabled = try(each.value.s3_log_encryption_disabled, var.s3_log_encryption_disabled)
+      }
+    } 
   }
 
   source {
@@ -78,7 +98,26 @@ resource "aws_codebuild_project" "infrastructure" {
   }
 
     tags = merge(
-        var.tags,
-        try(each.value.tags, null)
+      var.common_tags, 
+      try(each.value.tags, null)
     )
+}
+
+resource "aws_codebuild_webhook" "infrastructure" {
+  for_each = try({for build in var.builds: build.name => build if build.webhook_filter_groups != null}, toset(var.webhook_filter_groups))
+  project_name = aws_codebuild_project.infrastructure[each.key].name
+
+  dynamic "filter_group" {
+    for_each = try(each.value.webhook_filter_groups, var.webhook_filter_groups)
+    content {
+      dynamic "filter" {
+        for_each = filter_group.value
+        content {
+          type = filter.value.type
+          pattern = filter.value.pattern
+          exclude_matched_pattern = filter.value.exclude_matched_pattern
+        }
+      }
+    }
+  }
 }
--- terraform-aws-codebuild/variables.tf
+++ terraform-aws-codebuild/variables.tf
@@ -1,25 +1,50 @@
-variable "trigger_branch" {
-    default = "master"
-}
+#### GLOBAL-BUILD-PARAMETERS ####
 
 variable "resource_prefix" {
-    type = string
-    default = ""
+  description = "Prefix to use for all resource names"
+  type = string
+  default = ""
 }
 
 variable "resource_suffix" {
-    type = string
-    default = ""
+  description = "Suffix to use for all resource names"
+  default = ""
 }
 
 variable "terragrunt_version" {
+    description = "Terragrunt version to download within the build's respective buildspec.yml"
     type = string
     default = "0.25.4"
 }
 
 variable "terraform_version" {
+    description =  "Terraform version to download within the build's respective buildspec.yml"
     type = string
     default = "0.13.5"
+}
+
+ variable "common_tags" {
+     description = "Tags to attach to all builds"
+     type = map
+     default = null
+ }
+
+#### SINGLE-BUILD||DEFAULT-BUILD-PARAMETERS ####
+ 
+variable "name" {
+    description = "Build name (before adding var.resource_prefix and var.resource_suffix)"
+    type = string
+    default = null
+}
+
+variable "webhook_filter_groups" {
+    description = "Webhook filter groups to apply to the build. (only used when var.builds is null)"
+    type = list(list(object({
+        pattern = string
+        type = string
+        exclude_matched_pattern = bool
+    })))
+    default = []
 }
 
 variable "source_type" {
@@ -28,6 +53,11 @@ variable "source_type" {
 }
 
 variable "target_paths" {
+    type = list
+    default = null
+}
+
+variable "commands" {
     type = list
     default = null
 }
@@ -42,19 +72,104 @@ variable "buildspec" {
     default = "buildspec_tf_default.yml"
 }
 
+variable "artifacts" {
+    type = map
+    default = null
+}
+
+variable "build_compute_type" {
+    type = string
+    default = "BUILD_GENERAL1_SMALL"
+}
+
+variable "build_image" {
+    type = string
+    default = "aws/codebuild/standard:4.0"
+}
+
+variable "build_type" {
+    type = string
+    default = "LINUX_CONTAINER"
+}
+
+variable "build_image_pull_credentials_type" {
+    type = string
+    default = "CODEBUILD"
+}
+
+variable "s3_log_path" {
+    type = string
+    default = null
+}
+
+variable "s3_log_encryption_disabled" {
+    type = bool
+    default = false
+}
+
+variable "cw_group_name" {
+    type = string
+    default = null
+}
+
+variable "cw_stream_name" {
+    type = string
+    default = null
+}
+
+variable "service_role_arn" {
+    type = string
+    default = null
+}
+
+variable "environment_variables" {
+    type = list(object({
+        name = string
+        value = string
+        type = string
+    }))
+    default = []
+}
+
+#### MULTI-BUILD-PARAMETERS ####
+
 variable "builds" {
     type = any
+    default = null
+
 /*
 TODO: 
 change to below when issue: https://github.com/hashicorp/terraform/issues/19898 is fixed to allow optional map input
 type = list(object({
     name = string
     trigger_branch = string
-    target_paths = list
-    source_type = bool
-    commands = list
+    target_paths = list(string)
+    source_type = string
+    source_location = string
+    git_clone_depth = string
+    buildspec = string
+    commands = list(string)
     service_role_arn = string
-    environment_variables = {}
+    artifacts = map(string)
+    build_compute_type = string
+    build_image = string
+    build_type = string
+    build_image_pull_credentials_type = string
+    environment_variables = list(object({
+        name = string
+        value = string
+        type = string
+    }))
+    cw_stream_name = string
+    cw_group_name = string
+    s3_log_path = string
+    s3_log_encryption_disabled = bool
+    tags = map(string)
+    webhook_filter_groups = list(list(object({
+        pattern = string
+        type = string
+        exclude_matched_pattern = bool
+    })))
 }))
 */
 }
