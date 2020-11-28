fdacddb68ece937f373c33b2b6a7b01aaf6f2909
marshall7m <marshallmamiya@gmail.com>
2020-11-27 20:05:33 -0800

    initial terraform-aws-tf-codebuild module

---
+++ terraform-aws-tf-codebuild/outputs.tf
@@ -0,0 +1,11 @@
+output "build_arn" {
+    value = aws_codebuild_project.this.arn
+}
+
+output "build_name" {
+    value = aws_codebuild_project.this.name
+}
+
+output "role_arn" {
+    value = aws_iam_role.this[0].arn
+}
